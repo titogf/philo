a354507e7b874f9b2d13dd94dd49086e7d2e2fdc
Gonzalo FA <gonchogfa@gmail.com>
2023-06-18 21:24:07 +0200

    check function correction

--- pthreads.c
+++ pthreads.c
@@ -72,16 +72,21 @@ static void	*ft_pthread(void *data)
 
 static int	ft_check(t_philo *ph)
 {
-	pthread_mutex_lock(&ph->a->mutex_death);
 	if (ph->a->stop_process == 3)
 	{
+		pthread_mutex_lock(&ph->a->mutex_death);
 		pthread_mutex_lock(&ph->a->write_stats);
+		ph->a->stop_process = 0;
 		ft_print_stats(ph, "died");
 		pthread_mutex_unlock(&ph->a->write_stats);
+		ph->a->stop_process = 1;
 		pthread_mutex_unlock(&ph->a->mutex_death);
 		return (1);
 	}
-	else if (ph->a->nb_finished == ph->a->total_ph)
+	if (ph->a->stop_process)
+		return (1);
+	pthread_mutex_lock(&ph->a->mutex_death);
+	if (ph->a->nb_finished == ph->a->total_ph)
 	{
 		pthread_mutex_lock(&ph->a->write_stats);
 		ph->a->stop_process = 2;
